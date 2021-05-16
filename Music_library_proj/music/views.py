4608b035fea4b8d8d9517fd8b4803f07d0dba552
Nessiah919 <nesmith.charlesii@gmail.com>
2021-05-16 03:54:35 -0400

    created 'update', 'delete', and 'get' methods to 'SongDetail' class of views.py in music app. Tested each method in Postman. Each method checks out

--- Music_library_proj/music/views.py
+++ Music_library_proj/music/views.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 # Create your views here.
 class SongList(APIView):
+
     def get(self, request):
         song = Song.objects.all()
         serializer = SongSerializer(song, many=True)
@@ -23,10 +24,21 @@ class SongList(APIView):
 
 
 class SongDetail(APIView):
-    def put(self, song_id, request):
-        song = Song.objects.get(pk=song_id)
-        serializer = SongSerializer(song, data=request.data)
+
+    def get(self, request, pk):
+        song = Song.objects.get(pk=pk)
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        song_result = Song.objects.get(pk=pk)
+        serializer = SongSerializer(song_result, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        song = Song.objects.get(pk=pk)
+        song.delete()
+        return Response(status=status.HTTP_200_OK)
