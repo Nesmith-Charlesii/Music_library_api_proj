ee7533891de6fae2844dd6bd5072e0be9bd8512a
Nessiah919 <nesmith.charlesii@gmail.com>
2021-05-16 02:20:22 -0400

    created class 'SongDetail'. created 'put' method to query for requested song_id param, update song with requested id, and save changes to music table

--- Music_library_proj/music/views.py
+++ Music_library_proj/music/views.py
@@ -14,8 +14,19 @@ class SongList(APIView):
         return Response(serializer.data)
 
     def post(self, request):
+        # 'request' is the object sent from the client
         serializer = SongSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SongDetail(APIView):
+    def put(self, song_id, request):
+        song = Song.objects.get(pk=song_id)
+        serializer = SongSerializer(song, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
